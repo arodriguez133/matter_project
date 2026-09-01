3bfc69040068cf26e33adbf0e534fdf112adca7d
arodriguez133 <arodriguez133@icloud.com>
2026-08-31 22:30:28 -0500

    HMR infrastructure is scaffolded

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 import express from 'express';
 import path from 'path';
 import {fileURLToPath} from 'url';
+import webpackkDevMiddleware from 'webpack-dev-middleware';
+import webpack from 'webpack';
+import webPackConfig from './webpack.config.js'; 
+import webpackHotMiddleware from 'webpack-hot-middleware';
 
 const PORT = 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
+const compiler = webpack(webPackConfig);
 const app = express();
 
+app.use(webpackkDevMiddleware(compiler));
+app.use(webpackHotMiddleware(compiler));
 app.use(express.static(path.join(__dirname, 'dist'), {index: false}));
 
 app.get('/', (req, res)=>{
@@ -17,7 +23,4 @@ app.get('/', (req, res)=>{
 app.listen(PORT, () => {
   console.log(`Matter js app listening on ${PORT}`);
 });
-
-
-
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,22 @@
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import webpack from 'webpack';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default {
-  entry: {
-    index: './src/index.js',
-  },
+  mode: 'development',
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Matter js',
     }),
+    new webpack.HotModuleReplacementPlugin(),
   ],
+  entry: {
+    main: ['webpack-hot-middleware/client', './src/index.js'],
+  },
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
